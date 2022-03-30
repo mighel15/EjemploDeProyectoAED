77db52c14b344a0dd8279921d07b290e127d2ccf
Miguel Aceituno Rojo <Miguel Aceituno Rojo@DESKTOP-D73J91I>
2022-03-30 10:02:23 -0500

    Mi cambio

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main()
 {
-    int a = 5;
-    int b = 45;
-    cout << "La suma es: " << a + b << endl;
-    cout << "Hello World!\n";
+	int a = 5;
+	int b = 45;
+	cin >> a >> b;
+	cout << "La suma es: " << a + b << endl;
+	cout << "Hello World!\n";
 
 }
