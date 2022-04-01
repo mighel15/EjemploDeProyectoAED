6de62062dbd813e584a9b248aeaab409a9368d52
Miguel Aceituno Rojo <Miguel Aceituno Rojo@DESKTOP-D73J91I>
2022-04-01 11:01:44 -0500

    Factorial de un numero

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,57 @@
-#include <iostream>
+#include <iostream>//input output stream 
+//cin ingreso de valores
+//cout salida de valores
+
 using namespace std;
+
+int Factorial(int numero)
+{
+	int resultado = 1;
+
+	if (numero < 0)
+	{
+		cout << "El numero es negativo, por lo que no se puede calcular" << endl;
+	}
+	else
+	{
+		//f(n)=1*2*3*4*5*....n
+		for (int i = 1; i <= numero; i++)
+		{
+			resultado = resultado * i;
+		}
+		return resultado;
+	}
+}
 
 int main()
 {
-	int a = 5;
-	int b = 45;
-	cin >> a >> b;
-	cout << "La suma es: " << a + b << endl;
-	cout << "Hello World!\n";
-
+	int cantidadVeces;
+	int numero;
+	cout << "Indique la cantidad de factoriales que desea calcular: " << endl;
+	cin >> cantidadVeces;
+	for (int j = 0; j < cantidadVeces; j++)
+	{
+		cout << "Ingrese el número: " << endl;
+		cin >> numero;		
+		cout << "El factorial de " << numero << " es " << Factorial(numero) << endl;
+	}	
 }
+//EN GITHUB
+//Tarea:
+//Entrada: mi programa debe recibir la cantidad de numero fibonacis a generar
+//Salida: La serie finabonaci
+//Ejemplo
+//Entrada: 10
+//Salida: 1 - 1 - 2 - 3 - 5 - 8 - 13 - 21 - 34 - 55
+//Entrada: 5
+//Salida: 1 - 1 - 2 - 3 - 5
+//Entrada: -9
+//Salida: "No puedo procesar negativos"
+
+
+
+
+
+
+
+
